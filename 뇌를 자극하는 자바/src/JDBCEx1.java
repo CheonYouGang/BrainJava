11e5ada122656beff5020556357ec3acbf500033
kosea112 <kosea112@kosea112-PC>
2018-05-04 11:15:54 +0900

    JDBC수정 및 예시

--- 뇌를 자극하는 자바/src/JDBCEx1.java
+++ 뇌를 자극하는 자바/src/JDBCEx1.java
@@ -1,32 +1,34 @@
-import java.sql.Connection;//패키지
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-/**
- * 2018. 5. 3. Dev By Cheon You Gang
-   
-   JDBCExample1.java
- */
-
-/**
- * @author kosea112
- *
- */
-class JDBCExample1 {
+class JDBCEx1 {
     public static void main(String args[]) {
     	//Connection- 1단계: DB연결을 위한 커넥션 인터페이스
         Connection conn = null;
+        //Statement 인터페이스: SQL을 실행하는 객체
+        Statement stmt = null;
+        //ResultSet 인터페이스: SQL결과를 저장하는 객체
+        ResultSet rs = null;
+        
         //try~catch문에서 DB연결중 예외가 발생하는지 검사.
         try {
         	//Class.forName - 2단계: JDBC드라이버를 로드
             Class.forName("com.mysql.jdbc.Driver");
             //DriverManager - 3단계: 드라이버매니저 클래스는 getConnection메소드로 DB를 연결한다.
             conn = DriverManager.getConnection(
-            //"DBMS의 URL", "포트", "포트비밀번호"
+           /* "DBMS의 URL", "포트", "포트비밀번호"
+            getConnection(String url, String user, String password);*/
                "jdbc:mysql://localhost:3306/mysql", "root", "12345");
-            //{		}
             System.out.println("데이터베이스에 접속했습니다.");
-            //{		}
+
+            //커넥션 객체가 Statement객체를 생성
+            stmt = conn.createStatement();
+            //executeQuery DML쿼리 실행후 결과 저장
+            rs = stmt.executeQuery("select Jumincd, PName, Gender, Age from persons");            
+            
             //4단계 .close(); - DB연결 종료.
             conn.close();
         }
