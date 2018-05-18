96c6d5ae7cb0328611dcd286104626052bfef6fd
kosea112 <kosea112@kosea112-PC>
2018-05-18 14:40:08 +0900

    인터페이스

---
+++ 뇌를 자극하는 자바/src/Chap06/ColorPointEx.java
@@ -0,0 +1,54 @@
+/**
+ * 2018. 5. 16. Dev By Cheon You Gang
+   Chap06
+   ColorPointEx.java
+ */
+package Chap06;
+
+
+
+/**
+  * @author kosea112
+  *
+  */
+class Point{
+	private int x, y;
+	
+	public void set(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	public void showPoint() {
+		System.out.println("(" +x+ "," +y+ ")");
+	}
+}
+
+class ColorPoint extends Point{
+	private String color;
+	
+	public void setColor(String color) {
+		this.color=color;
+	} 
+	public void showColorPoint() {
+		System.out.println(color);
+		showPoint();
+	}
+}
+
+public class ColorPointEx {
+
+	public static void main(String[] args) {
+		
+		Point p = new Point();
+		p.set(1, 2);
+		p.showPoint();
+		
+		System.out.println("");
+		
+		ColorPoint cp = new ColorPoint();
+		cp.set(3, 4);
+		cp.setColor("red");
+		cp.showColorPoint();
+	}
+
+}
