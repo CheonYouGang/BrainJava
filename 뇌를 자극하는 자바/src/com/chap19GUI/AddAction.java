3c735379927e65ae8645ed7dcfb1977feda331b8
kosea112 <kosea112@kosea112-PC>
2018-05-30 17:52:21 +0900

    jdbcManager 기능 추가, DB삽입(추가)기능 추가

--- 뇌를 자극하는 자바/src/com/chap19GUI/AddAction.java
+++ 뇌를 자극하는 자바/src/com/chap19GUI/AddAction.java
@@ -7,10 +7,12 @@ package com.chap19GUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.sql.ResultSet;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
+
+import com.kosea.kmove30.Jdbc_Manager;
 
 /**
  * @author kosea112
@@ -18,10 +20,14 @@ import javax.swing.table.DefaultTableModel;
  */
 public class AddAction implements ActionListener {
 	JTable table;
+	Jdbc_Manager jdbcManager;
 	JTextField text1, text2, text3;
+	ResultSet rs;
 
-	public AddAction(JTable table, JTextField text1, JTextField text2, JTextField text3) {
+	public AddAction(JTable table, Jdbc_Manager jdbcManager, JTextField text1, JTextField text2, JTextField text3) {
+		super();
 		this.table = table;
+		this.jdbcManager = jdbcManager;
 		this.text1 = text1;
 		this.text2 = text2;
 		this.text3 = text3;
@@ -34,6 +40,7 @@ public class AddAction implements ActionListener {
 		arr[0] = text1.getText();
 		arr[1] = text2.getText();
 		arr[2] = text3.getText();
+		//DefaultTableModel - 2차원 배열로 표현한 GUI를 유동적으로 보여주기 위한 객체
 		DefaultTableModel model = (DefaultTableModel) table.getModel();
 		
 		boolean isCheck = false;// 입력창 체크 상태
@@ -45,9 +52,14 @@ public class AddAction implements ActionListener {
 				isCheck = false;
 		}
 		
-		if(isCheck)
+		if(isCheck) {
 			model.addRow(arr);// 레코드 데이터 추가
-
+			try {
+				jdbcManager.Insert(arr);//DBInsert 추가
+			} catch (Exception e2) {
+				e2.getMessage();
+			}
+		}
 		// 입력창 빈칸 만들기
 		text1.setText("");
 		text2.setText("");
