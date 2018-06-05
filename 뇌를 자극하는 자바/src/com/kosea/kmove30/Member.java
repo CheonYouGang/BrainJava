e8fa5e2b22e485b066f55c69a23e3f4ae4d9d9ad
kosea112 <kosea112@kosea112-PC>
2018-06-05 18:59:08 +0900

    마이바티스를 이용한 jdbc연결. jdbc:mysql://localhost:3306/mysql

---
+++ 뇌를 자극하는 자바/src/com/kosea/kmove30/Member.java
@@ -0,0 +1,53 @@
+/**
+ * 2018. 6. 5. Dev By Cheon You Gang
+   com.kosea.kmove30
+   Member.java
+ */
+package com.kosea.kmove30;
+
+ /**
+  * @author kosea112
+  *
+  */
+public class Member {
+	int mno;
+	String LID;
+	String LPASSWORD;
+	
+	//* public Member()를 만들거나 혹은 전부 없애거나 해야 한다.
+	public Member() {
+		//기본 생성자.
+	}
+
+	public Member(int mno, String lID, String lPASSWORD) {
+		super();
+		this.mno = mno;
+		LID = lID;
+		LPASSWORD = lPASSWORD;
+	}
+
+	public int getMno() {
+		return mno;
+	}
+
+	public String getLID() {
+		return LID;
+	}
+
+	public String getLPASSWORD() {
+		return LPASSWORD;
+	}
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+	public void setLID(String lID) {
+		LID = lID;
+	}
+
+	public void setLPASSWORD(String lPASSWORD) {
+		LPASSWORD = lPASSWORD;
+	}
+
+}
---
+++ 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
@@ -0,0 +1,43 @@
+/**
+ * 2018. 6. 5. Dev By Cheon You Gang
+   com.kosea.kmove30
+   MybatisTest.java
+ */
+package com.kosea.kmove30;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+  * @author kosea112
+  *
+  */
+public class MybatisTest {
+
+	public static void main(String[] args) {
+		
+		String resource = "mybatis-config.xml";//src경로
+		
+		try {
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+			SqlSession session = sqlSessionFactory.openSession();
+			try {
+			  // Member member = new Member(101); = 밑에 있는 명령문과 같은 역할
+				Member member  = (Member)session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
+			    System.out.println("회원 아이디: " + member.getLID());
+			} finally {
+			  session.close();
+			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
