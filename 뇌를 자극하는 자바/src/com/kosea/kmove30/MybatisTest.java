cf81809eea1b708caab0e3a953e1faf1c5abd158
kosea112 <kosea112@kosea112-PC>
2018-06-07 16:48:08 +0900

    매퍼의 수정, 삽입 추가

--- 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
+++ 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
@@ -30,12 +30,27 @@ public class MybatisTest {
 			try {
 			  // Member member = new Member(101); = 밑에 있는 명령문과 같은 역할
 			  // select구문
-				Member member  = (Member)session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
-			    System.out.println("회원 아이디: " + member.getLID());
+/*				Member member  = (Member)session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
+			    System.out.println("회원 아이디: "   + member.getLID());
+			    System.out.println("회원 비밀번호: " + member.getLPASSWORD());
+			    System.out.println("회원 번호: "	 + member.getMno());*/
 			    
 			  // delect구문
-			    int deleteCount = session.delete("org.mybatis.example.SelectMapper.deleteMember", 101);
-			    System.out.println("삭제건수"+deleteCount);
+/*			    int deleteCount = session.delete("org.mybatis.example.SelectMapper.deleteMember", 101);
+			    System.out.println("삭제건수"+deleteCount);*/
+			    
+			  // insert구문
+/*			    Member newMember = new Member("천유강", "0123456789!", 103);
+			    int insertCount = session.insert("org.mybatis.example.SelectMapper.insertMember", newMember);
+			    System.out.println("추가 건수" + insertCount);*/
+			    
+			  // update 구문
+			    String queryMapping = "org.mybatis.example.SelectMapper.updateMember";
+			    Member member = new Member(102, "zxcvb", "1234567890"
+			    		+ "!");
+			    int updateCount = session.update(queryMapping, member);		
+			    System.out.println("수정건수" + updateCount);
+			    
 			    
 			} catch (Exception e) {
 				System.out.println(e.getMessage());
