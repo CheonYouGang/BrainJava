c5028f3535aec5329fd06395ad6832919989e309
kosea112 <kosea112@kosea112-PC>
2018-06-07 15:02:59 +0900

    delete 마이바티스 sql 추가

--- 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
+++ 뇌를 자극하는 자바/src/com/kosea/kmove30/MybatisTest.java
@@ -29,9 +29,18 @@ public class MybatisTest {
 			SqlSession session = sqlSessionFactory.openSession();
 			try {
 			  // Member member = new Member(101); = 밑에 있는 명령문과 같은 역할
+			  // select구문
 				Member member  = (Member)session.selectOne("org.mybatis.example.SelectMapper.selectMember", 101);
 			    System.out.println("회원 아이디: " + member.getLID());
-			} finally {
+			    
+			  // delect구문
+			    int deleteCount = session.delete("org.mybatis.example.SelectMapper.deleteMember", 101);
+			    System.out.println("삭제건수"+deleteCount);
+			    
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}finally {
+			  session.commit();
 			  session.close();
 			}
 			
